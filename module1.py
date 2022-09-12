0ed8f8cc941e5850db56b5ac9366086c1e03d378
rsilcas <rene.silva.castillo@gmail.com>
2022-09-12 18:52:26 -0300

    TEST

---
+++ module1.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      rsilc
+#
+# Created:     12/09/2022
+# Copyright:   (c) rsilc 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    print("LEYENDO PDF")
+    pass
+
+if __name__ == '__main__':
+    main()
