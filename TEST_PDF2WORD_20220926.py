28959a78bad56a41b77f2733610e85172fd0344f
rsilcas <rene.silva.castillo@gmail.com>
2022-10-13 16:33:30 -0300

    UPDATING CV

--- TEST_PDF2WORD_20220926.py
+++ TEST_PDF2WORD_20220926.py
@@ -8,9 +8,25 @@
 # Copyright:   (c) rsilc 2022
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+from pdf2docx import Converter
+
+pdf_file = 'PROFILE_.pdf'
+
+doc_file= 'OUT_PDF2WORD.docx'
+
+
 
 def main():
     print("LEYENDO PDF")
+    c=Converter(pdf_file)
+
+    c.convert(doc_file)
+    c.close()
+
+
+    print("FIN LEYENDO PDF")
+
+
     pass
 
 if __name__ == '__main__':
